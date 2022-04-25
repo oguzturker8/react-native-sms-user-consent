0582906abaabb5ef7fc3043870dd9ea327593e48
oguzturker8 <oguzturker8@gmail.com>
2022-04-25 12:40:30 +0300

    revert

--- android/src/main/java/ua/kyivstar/reactnativesmsuserconsent/SmsUserConsentModule.kt
+++ android/src/main/java/ua/kyivstar/reactnativesmsuserconsent/SmsUserConsentModule.kt
@@ -35,12 +35,12 @@ class SmsUserConsentModule(reactContext: ReactApplicationContext) : ReactContext
     }
     this.promise = promise
     if (reactContext?.currentActivity != null) {
-      val task: Task<Void> = SmsRetriever.getClient(reactContext.currentActivity!!).startSmsUserConsent(null)
+        val task: Task<Void> = SmsRetriever.getClient(reactContext.currentActivity!!).startSmsUserConsent(null)
 
-      task.addOnSuccessListener { // successfully started an SMS Retriever for one SMS message
-        registerReceiver()
-      }
-      task.addOnFailureListener { e -> promise.reject(E_OTP_ERROR, e) }
+        task.addOnSuccessListener { // successfully started an SMS Retriever for one SMS message
+          registerReceiver()
+        }
+        task.addOnFailureListener { e -> promise.reject(E_OTP_ERROR, e) }
     }
   }
 
@@ -52,9 +52,9 @@ class SmsUserConsentModule(reactContext: ReactApplicationContext) : ReactContext
 
   private fun registerReceiver() {
     if (reactContext?.currentActivity != null) {
-      receiver = SmsRetrieveBroadcastReceiver(reactContext.currentActivity)
-      val intentFilter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
-      reactContext.currentActivity?.registerReceiver(receiver, intentFilter, SmsRetriever.SEND_PERMISSION, null)
+        receiver = SmsRetrieveBroadcastReceiver(reactContext.currentActivity)
+        val intentFilter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
+        reactContext.currentActivity?.registerReceiver(receiver, intentFilter, SmsRetriever.SEND_PERMISSION, null)
     }
   }
 
